11e384c5528f951d54ec0ef7bab1c227bfc25290
Dorialean <lana.lakutina@mail.ru>
2022-07-13 17:09:54 +0300

    half way there

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.sirius.test_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //Если бы хотел грузить картинки через DataModel, то сделал бы это так:
+    private fun getBitmapFromURL(src: String): Bitmap? {
+        return try {
+            Log.e("src", src)
+            val url = URL(src)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.setDoInput(true)
+            connection.connect()
+            val input: InputStream = connection.getInputStream()
+            val myBitmap = BitmapFactory.decodeStream(input)
+            Log.e("Bitmap", "returned")
+            myBitmap
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
     }
 }
