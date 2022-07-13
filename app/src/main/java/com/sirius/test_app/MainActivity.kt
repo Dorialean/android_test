5722e73a75fcb8ec8c0c0bb5a32c320b8b01aa91
Dorialean <lana.lakutina@mail.ru>
2022-07-13 21:23:20 +0300

    button  and user2 added

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -20,9 +21,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val dotaLogo : ImageView = findViewById(R.id.logoImageView)
         val mainConstLayout = findViewById<ConstraintLayout>(R.id.mainContraintLayout)
         mainConstLayout.bringChildToFront(dotaLogo)
+
+        val installButton = findViewById<Button>(R.id.installButton)
+        installButton.text.asIterable().forEach(){
+            run {
+                if (it != 'I')
+                    it.uppercaseChar()
+            }
+        }
     }
 
     //Если бы хотел грузить картинки через DataModel, то сделал бы это так:
